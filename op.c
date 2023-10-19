6addf42f6d75cd5d5dfe429646b03baae357245d
Est-1y <133119207+Est-1y@users.noreply.github.com>
2023-10-19 13:40:50 +0300

    Create op.c

---
+++ op.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+
+/**
+ * mul_nodes - Adds top two elements of the stack
+ * @stack: Points to top node of the stack
+ * @line_number: line number of the opcode
+ */
+void mul_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "mul");
+
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n * (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
+/**
+ * mod_nodes - Adds top two elements of the stack
+ * @stack: Points to top node of the stack
+ * @line_number: line number of of the opcode
+ */
+void mod_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+
+		more_err(8, line_number, "mod");
+
+
+	if ((*stack)->n == 0)
+		more_err(9, line_number);
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n % (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
