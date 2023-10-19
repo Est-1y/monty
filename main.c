f5c2cbdad48683556f7624409807dab120ad787f
Est-1y <terryiana22@gmail.com>
2023-10-19 03:22:45 -0700

    mony

--- main.c
+++ main.c
@@ -1,40 +1,82 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define MAX_LINE_LENGTH 1024
+#include "monty.h"
 
 /**
- * main - entry
- * @argc: argument count
- * @argv: argument value
- * Return: 0 (success)
+ * main - entry point
+ * @argc: arguments count
+ * @argv: list of arguments
+ * Return: always 0
  */
 
 int main(int argc, char *argv[])
 {
-	FILE *file;
-	char content[MAX_LINE_LENGTH];
-
 	if (argc != 2)
 	{
-		printf("Usage: %s filename\n", argv[0]);
-		return (1);
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
 	}
-
-
-	file = fopen(argv[1], "r");
-	if (!file)
-	{
-		perror("Error opening file");
-		return (1);
-	}
-
-	while (fgets(content, MAX_LINE_LENGTH, file))
-	{
-		printf("%s", content);
-	}
-
-	fclose(file);
-
+	open_file(argv[1]);
+	free_nodes();
 	return (0);
+}
+
+/**
+ * create_node - Creates a node in stack
+ * @n: Number
+ * Return: pointer to the node
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * free_nodes - Free nodes in stack
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+
+/**
+ * add_to_queue - Adds nodes to queue
+ * @new_node: Points to the new node
+ * @ln: line number
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+
 }
