1abfa06c3e1be8c13d8582afa624a65fa095964e
Est-1y <133119207+Est-1y@users.noreply.github.com>
2023-10-19 11:29:32 +0300

    Create nopcode.c

---
+++ nopcode.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- void element
+  *@head: the stack head
+  *@counter: line_number counter
+  *Return: no return.
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
